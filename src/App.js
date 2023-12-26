eaa3c41290cd856b29c3187cdc8612fdd4e06f25
Ravindra Singhai <ravindra.singhai@gmail.com>
2023-12-26 13:34:49 -0600

    awsamplify-demo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hellow and Welcome ! Have a happy learning...
         </p>
         <a
           className="App-link"
